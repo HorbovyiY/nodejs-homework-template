bb074daddd5c0bda5eba18da06a2259463adecd3
HorbovyiY <yhorbovyi@gmail.com>
2023-06-13 08:15:38 +0300

    Додає post запит

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,12 +2,19 @@ const express = require('express');
 const contactsFunctions = require('../../models/contacts');
 const router = express.Router();
 const { HttpError } = require("../../helpers");
+const Joi = require("joi");
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(), 
+  phone: Joi.number().required(),
+})
 
 
 router.get('/', async (req, res, next) => {
   try {
     const allContacts = await contactsFunctions.listContacts();
-    res.json(allContacts);
+    res.status(200).json(allContacts);
   } catch (error) { 
     next(error);
   }
@@ -19,13 +26,18 @@ router.get('/:contactId', async (req, res, next) => {
     if (!contact) { 
       throw HttpError(404, "Not Found");
     }
-    res.json(contact);
+    res.status(200).json(contact);
   } catch (error) {
     next(error);
   }})
 
 router.post('/', async (req, res, next) => {
   try {
+    const { error } = addSchema.validate(req.body);
+    if (error) { 
+      throw HttpError(400, "missing required name field");
+    }
+
     const newContact = await contactsFunctions.addContact(req.body);
     res.status(201).json(newContact);
   } catch (error) {
