9b78c284db0d354709f6efd3f635e6517e500ff6
HorbovyiY <yhorbovyi@gmail.com>
2023-06-17 19:18:33 +0300

    додає роутер current

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { validateBody } = require("../../middlewares");
+const { validateBody, authenticate } = require("../../middlewares");
 const { schemas } = require("../../models/user");
 const ctrl = require("../../controllers/auth");
 
@@ -9,5 +9,7 @@ const router = express.Router();
 router.post("/register", validateBody(schemas.authSchema), ctrl.register);
 
 router.post("/login", validateBody(schemas.authSchema), ctrl.login);
+
+router.get("/current", authenticate, ctrl.getCurrent);
 
 module.exports = router;
