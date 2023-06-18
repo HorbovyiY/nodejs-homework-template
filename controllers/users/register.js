b3c0ccbd4714dff994d45dd89596372fd5b18c9e
HorbovyiY <yhorbovyi@gmail.com>
2023-06-18 13:31:53 +0300

    створює контролер updateAvatar

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const gravatar = require("gravatar");
 
 const { User } = require("../../models/user");
 const { ctrlWrapper, HttpError } = require("../../helpers");
@@ -12,8 +13,9 @@ const register = async (req, res) => {
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
+    const avatarURL = gravatar.url(email);
 
-    const newUser = await User.create({ ...req.body, password: hashPassword });
+    const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
     const newUserResult = { user: { email: newUser.email, subscription: newUser.subscription } };
     res.status(201).json(newUserResult);
 }
