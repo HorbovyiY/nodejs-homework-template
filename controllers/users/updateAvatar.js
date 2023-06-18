ff39dbd5773576bbbc44e146d0df71ca056afa8d
HorbovyiY <yhorbovyi@gmail.com>
2023-06-18 14:05:41 +0300

    додає обробку розмірів аватарки

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const { ctrlWrapper } = require("../../helpers");
 const { User } = require("../../models/user");
@@ -11,7 +12,13 @@ const updateAvatar = async (req, res) => {
     const { path: tempUpload, originalname } = req.file;
     const filename = `${_id}_${originalname}`;
     const resultUpload = path.join(avatarsDir, filename);
+
     await fs.rename(tempUpload, resultUpload);
+
+    const image = await Jimp.read(`public/avatars/${filename}`);
+    await image.resize(250, 250);
+    await image.writeAsync(`public/avatars/${filename}`);
+
     const avatarURL = path.join("avatars", filename);
     await User.findByIdAndUpdate(_id, { avatarURL });
     res.json({
